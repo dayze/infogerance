e17c6cfb5d95a990bd8523a0e094bf95f6c826f3
Dez <elye24@gmail.com>
2017-11-28 10:57:57 +0100

    test if user exist

--- newUser.js
+++ newUser.js
@@ -12,6 +12,8 @@ if (user === '') {
 // check if an user is not already present in the system
 getAsync(`getent passwd ${user}`)
   .then((err, data, stderr) => {
+  console.log("data: ", data)
+  console.log("stderr: ", stderr)
   if (data.trim() === '0') {
     console.log('good, no user are present with this username')
   }
