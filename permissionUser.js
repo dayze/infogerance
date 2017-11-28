eec5cf400ca1341d6ccbfa69e29efe706021612b
Dez <elye24@gmail.com>
2017-11-28 15:12:42 +0100

    ok

--- permissionUser.js
+++ permissionUser.js
@@ -7,9 +7,9 @@ module.exports.apply =  (folderOfUser, userName) => {
   child_process.exec(`chmod 700 ${folderOfUser}/private`)
   child_process.exec(`chmod 700 ${folderOfUser}/www-dev`)
   child_process.exec(`chmod 700 ${folderOfUser}/www-prod`)
-  child_process.exec(`chown ${folderOfUser}:sftp ${folderOfUser}/private`)
-  child_process.exec(`chown ${folderOfUser}:sftp ${folderOfUser}/www-dev`)
-  child_process.exec(`chown ${folderOfUser}:sftp ${folderOfUser}/www-prod`)
+  child_process.exec(`chown ${userName}:sftp ${folderOfUser}/private`)
+  child_process.exec(`chown ${userName}:sftp ${folderOfUser}/www-dev`)
+  child_process.exec(`chown ${userName}:sftp ${folderOfUser}/www-prod`)
   child_process.exec(`chown root:sftp ${folderOfUser}/log`)
   child_process.exec(`chmod 755 ${folderOfUser}/log`)
 }
